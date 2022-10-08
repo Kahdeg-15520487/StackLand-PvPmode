eb0f37f6c5535e3aa0eee7fa3c8eaaa0166b6beb
Merlin Nguyen <jonhenry13241@gmail.com>
2022-10-08 14:43:30 +0700

    init

---
+++ CustomMenu.cs
@@ -0,0 +1,26 @@
+﻿using BerryLoaderNS;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TMPro;
+using UnityEngine;
+
+namespace YourMod
+{
+    public class CustomMenu : MonoBehaviour
+    {
+        private CustomButton menuButton;
+
+        public void Start()
+        {
+            var log = BerryLoader.L;
+            log.LogInfo("initing screen..");
+
+            menuButton = MenuAPI.CreateButton(GameCanvas.instance.MainMenuScreen.GetChild(0).GetChild(5), "Mod Options", () =>
+            {
+                log.LogInfo("menu button clicked..");
+            });
+            menuButton.transform.SetSiblingIndex(2);
+        }
+    }
+}
--- YourMod.cs
+++ YourMod.cs
@@ -1,18 +1,37 @@
 using BepInEx;
 using BerryLoaderNS;
+using HarmonyLib;
+using UnityEngine;
 
 namespace YourMod
 {
-	[BepInPlugin("EpicExampleModId", "Epic Example Mod", "0.0.1")]
-	[BepInDependency("BerryLoader")]
-	class Plugin : BaseUnityPlugin
-	{
-		public static BepInEx.Logging.ManualLogSource L;
+    [BepInPlugin("pvpmod", "PvP mode", "0.0.1")]
+    [BepInDependency("BerryLoader")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static BepInEx.Logging.ManualLogSource L;
+        private Harmony HarmonyInstance;
 
-		private void Awake()
-		{
-			L = Logger;
-			L.LogInfo("hello from YourMod.Plugin.Awake");
-		}
-	}
+        private void Awake()
+        {
+            L = Logger;
+            L.LogInfo("awake pvpmod 1");
+            HarmonyInstance = new Harmony("pvpmod");
+            HarmonyInstance.PatchAll(typeof(Patches));
+            L.LogInfo("awake pvpmod 2");
+        }
+    }
+
+    public class Patches
+    {
+
+        [HarmonyPatch(typeof(GameCanvas), "Awake")]
+        [HarmonyPostfix]
+        public static void GCAPost()
+        {
+            MenuAPI.Init();
+            var m = new GameObject().AddComponent<CustomMenu>();
+            BerryLoader.L.LogInfo("hooking custom menu");
+        }
+    }
 }
